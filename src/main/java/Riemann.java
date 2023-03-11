3caf45dadbbb6582592f68541cf28719758f5da4
Artemij <artemijekazakov@yandex.ru>
2023-03-11 20:01:59 +0300

    final version

--- src/main/java/Riemann.java
+++ src/main/java/Riemann.java
@@ -5,17 +5,37 @@ public class Riemann {
     public static void main(String[] args) {
         RiemannSum sm = new RiemannSum(new MyFunction());
 
-        int n = 1000;
+        int n = 10;
         double l = 1;
         double r = 4;
 
+        double al = 25.5;
+
         System.out.println(new MyFunction());
-        System.out.printf("LeftPoint : %.6f\n", sm.calculateSum(n, l, r, Method.LeftPoint, true, true));
-        System.out.printf("MiddlePoint : %.6f\n", sm.calculateSum(n, l, r, Method.MiddlePoint, false, false));
-        System.out.printf("RightPoint : %.6f\n", sm.calculateSum(n, l, r, Method.RightPoint, true, false));
-        System.out.printf("RandomPoint : %.6f\n", sm.calculateSum(n, l, r, Method.RandomPoint, false, false));
-        System.out.printf("Trapezium : %.6f\n", sm.calculateSum(n, l, r, Method.Trapezium, false, false));
-        System.out.println();
+        System.out.printf("LeftPoint : %.6f\n",
+                sm.calculateSum(n, l, r, Method.LeftPoint, true, true));
+        System.out.printf("Error LeftPoint : %.6f\n\n",
+                Math.abs(al - sm.calculateSum(n, l, r, Method.LeftPoint, false, false)));
+
+        System.out.printf("MiddlePoint : %.6f\n",
+                sm.calculateSum(n, l, r, Method.MiddlePoint, true, false));
+        System.out.printf("Error MiddlePoint : %.6f\n\n",
+                Math.abs(al - sm.calculateSum(n, l, r, Method.MiddlePoint, false, false)));
+
+        System.out.printf("RightPoint : %.6f\n",
+                sm.calculateSum(n, l, r, Method.RightPoint, true, false));
+        System.out.printf("Error RightPoint : %.6f\n\n",
+                Math.abs(al - sm.calculateSum(n, l, r, Method.RightPoint, false, false)));
+
+        System.out.printf("RandomPoint : %.6f\n",
+                sm.calculateSum(n, l, r, Method.RandomPoint, true, false));
+        System.out.printf("Error RandomPoint : %.6f\n\n",
+                Math.abs(al - sm.calculateSum(n, l, r, Method.RandomPoint, false, false)));
+
+        System.out.printf("Trapezium : %.6f\n",
+                sm.calculateSum(n, l, r, Method.Trapezium, true, false));
+        System.out.printf("Error Trapezium : %.6f\n\n",
+                Math.abs(al - sm.calculateSum(n, l, r, Method.Trapezium, false, false)));
 
         // дополнительно
         l = -4;
