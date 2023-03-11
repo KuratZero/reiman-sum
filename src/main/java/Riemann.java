69e07cf340506f1b9617c56147eb9d75c61c9ba9
Artemij <artemijekazakov@yandex.ru>
2023-03-11 19:41:34 +0300

    final ?3 version

--- src/main/java/Riemann.java
+++ src/main/java/Riemann.java
@@ -5,16 +5,16 @@ public class Riemann {
     public static void main(String[] args) {
         RiemannSum sm = new RiemannSum(new MyFunction());
 
-        int n = 10;
+        int n = 1000;
         double l = 1;
         double r = 4;
 
         System.out.println(new MyFunction());
-        System.out.printf("LeftPoint : %.6f\n", sm.calculateSum(n, l, r, Method.LeftPoint, true, false));
-        System.out.printf("MiddlePoint : %.6f\n", sm.calculateSum(n, l, r, Method.MiddlePoint, true, false));
+        System.out.printf("LeftPoint : %.6f\n", sm.calculateSum(n, l, r, Method.LeftPoint, true, true));
+        System.out.printf("MiddlePoint : %.6f\n", sm.calculateSum(n, l, r, Method.MiddlePoint, false, false));
         System.out.printf("RightPoint : %.6f\n", sm.calculateSum(n, l, r, Method.RightPoint, true, false));
-        System.out.printf("RandomPoint : %.6f\n", sm.calculateSum(n, l, r, Method.RandomPoint, true, false));
-        System.out.printf("Trapezium : %.6f\n", sm.calculateSum(n, l, r, Method.Trapezium, true, false));
+        System.out.printf("RandomPoint : %.6f\n", sm.calculateSum(n, l, r, Method.RandomPoint, false, false));
+        System.out.printf("Trapezium : %.6f\n", sm.calculateSum(n, l, r, Method.Trapezium, false, false));
         System.out.println();
 
         // дополнительно
@@ -23,7 +23,7 @@ public class Riemann {
         RiemannSum sin = new RiemannSum(new SinFunc());
 
         System.out.println(new SinFunc());
-        System.out.printf("Trapezium : %.6f\n", sin.calculateSum(n, l, r, Method.Trapezium, true, false));
+        System.out.printf("Trapezium : %.6f\n", sin.calculateSum(n, l, r, Method.Trapezium, false, false));
 
 
 
